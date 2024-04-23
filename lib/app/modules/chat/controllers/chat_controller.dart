af9fdf0bd34ef72487619b075c7aa6f5ba97f8c3
Sveta <ghjk>
2024-04-23 17:14:22 +0500

    tochno work choto

--- lib/app/modules/chat/controllers/chat_controller.dart
+++ lib/app/modules/chat/controllers/chat_controller.dart
@@ -5,19 +5,26 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class ChatController extends GetxController {
- RxList<ChatMessage> get messages => UserService.to.messages;
-final textController=TextEditingController();
-final focusNode = FocusNode();
+  RxList<ChatMessage> get messages => UserService.to.messages;
+  final textController = TextEditingController();
+  final scrollController = ScrollController();
+  final focusNode = FocusNode();
   @override
   void onInit() {
+    messages.listen((p0) async {
+      await Future.delayed(Duration(milliseconds: 100), () {
+        scrollController.jumpTo(scrollController.position.maxScrollExtent);
+      });
+    });
     super.onInit();
   }
-disconnect()=>SocketService.to.disconnect();
-bool itsMe(String clientId)=>clientId ==  SocketService.to.clientId;
 
-void sendMessage(){
-  var message = textController.text;
-  if(messages.isNotEmpty)  SocketService.to.sendMessage(message);
-  textController.clear();
-}
+  disconnect() => SocketService.to.disconnect();
+  bool itsMe(String clientId) => clientId == SocketService.to.clientId;
+
+  void sendMessage() {
+    var message = textController.text;
+    if (messages.isNotEmpty) SocketService.to.sendMessage(message);
+    textController.clear();
+  }
 }
--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:chat/app/modules/chat/widget/bubble_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -11,46 +10,53 @@ class ChatView extends GetView<ChatController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ChatView'),
-        leading: IconButton(icon: Icon(Icons.exit_to_app), onPressed: ()=> controller.disconnect(),),
-      
-      ),
-      body: Column(
-        children: [
-          Expanded(child: 
-          Obx(()=>ListView.builder(itemBuilder: (Context, index){
-            var message=controller.messages[index];
-            var itsMe=controller.itsMe(message.client);
-            switch (message.type){
-              case SocketEvent.login:
-              return Text("${message.username} вошел в чат");
-              case SocketEvent.logout:
-              return Text("${message.username} покинул чат");
-              case SocketEvent.newMessage:
-              return BubbleWidget(message: message, itsMe: itsMe);
-              default:
-              return SizedBox();
-            }
-          },
-          itemCount: controller.messages.length,
-          )),
-          
+        appBar: AppBar(
+          title: const Text('ChatView'),
+          leading: IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () => controller.disconnect(),
           ),
-          Container(
-            width: 50,
-            decoration: BoxDecoration(color: Colors.blue),
-            child: TextField(
-              focusNode: controller.focusNode,
-              controller: controller.textController,
-              onSubmitted: (value) => controller.sendMessage(),
-              
-              decoration: InputDecoration(suffixIcon:IconButton( icon: Icon(Icons.send), onPressed: ()=>controller.sendMessage(),)
-              ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Obx(() => ListView.builder(
+                    controller: controller.scrollController,
+                    itemBuilder: (Context, index) {
+                      var message = controller.messages[index];
+                      var itsMe = controller.itsMe(message.client);
+                      switch (message.type) {
+                        case SocketEvent.login:
+                          return Text("${message.username} вошел в чат");
+                        case SocketEvent.logout:
+                          return Text("${message.username} покинул чат");
+                        case SocketEvent.newMessage:
+                          return BubbleWidget(message: message, itsMe: itsMe);
+                        default:
+                          return SizedBox();
+                      }
+                    },
+                    itemCount: controller.messages.length,
+                  )),
             ),
-          )
-        ],
-      )
-    );
+            Padding(padding:EdgeInsets.all(8.0),
+            child:
+            Container(
+             
+              child: TextField(
+                focusNode: controller.focusNode,
+                controller: controller.textController,
+                onSubmitted: (value) => controller.sendMessage(),
+                decoration: InputDecoration(
+                  hintText: 'сообщение... ',
+                    suffixIcon: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => controller.sendMessage(),
+                )),
+              ),
+              ),
+            )
+          ],
+        ));
   }
 }
