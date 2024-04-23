2fbf124933ca7e0b76d4cce078f61a6858944901
Sveta <ghjk>
2024-04-23 10:08:15 +0500

    socket service

--- lib/app/services/socket_service.dart
+++ lib/app/services/socket_service.dart
@@ -1,7 +1,18 @@
 import 'package:get/get.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
 class SocketService extends GetxService {
+  late Socket _socket;
   Future<SocketService> init() async {
+    _socket = io('https://masqed.ru',
+    OptionBuilder()
+    .setTransports(['websocket'])
+    .setPath('/chat/socket.io')
+    .disableAutoConnect()
+    .disableReconnection()
+    .build()
+    
+    );
     return this;
   }
 
